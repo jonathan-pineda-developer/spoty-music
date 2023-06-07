36e924a8fd798ab0f2c929b19a30c805f0fff93d
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-06-06 22:23:55 -0600

    agrego menu a cada elemento

--- app/src/main/java/cr/ac/una/spoty/entity/trackAdaprter.kt
+++ app/src/main/java/cr/ac/una/spoty/entity/trackAdaprter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
 import com.squareup.picasso.Picasso
 import cr.ac.una.spoty.entity.Track
@@ -29,6 +30,29 @@ class TrackAdapter(context: Context, tracks: List<Track>) : ArrayAdapter<Track>(
         txtArtistName?.text = track?.album?.name
 
         Picasso.get().load(track?.album?.images?.get(0)?.url).into(imgPhoto)
+        val imgMenu = convertView?.findViewById<ImageView>(R.id.imgAction)
+        imgMenu?.setOnClickListener {
+            val popupMenu = PopupMenu(context, imgMenu)
+            popupMenu.inflate(R.menu.main_menu)
+
+            // Agrega clics de escucha para las opciones del menú
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.menu_view_album -> {
+                        // Acción para Ver álbum
+                        true
+                    }
+                    R.id.menu_view_artis -> {
+                        // Acción para Ver artista
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
+        }
+
 
         return itemView!!
     }
