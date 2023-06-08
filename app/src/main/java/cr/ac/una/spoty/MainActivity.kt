bc053becbadeecaab148cd0dbb8a9f17b638a8f2
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-06-08 12:33:28 -0600

    agregando el patron view controller

--- app/src/main/java/cr/ac/una/spoty/MainActivity.kt
+++ app/src/main/java/cr/ac/una/spoty/MainActivity.kt
@@ -1,61 +1,41 @@
-package cr.ac.una.controlarterial.view
+package cr.ac.una.spoty
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.fragment.findNavController
-import cr.ac.una.controlarterial.R
-import cr.ac.una.controlarterial.databinding.ActivityMainBinding
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import cr.ac.una.spoty.controllers.TrackController
+import cr.ac.una.spoty.databinding.ActivityMainBinding
+import cr.ac.una.spoty.entity.TrackAdapter
 
 class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+        private lateinit var btnSearch: Button
+        private lateinit var txtSearch: EditText
+        private var listViewItems: ListView? = null
+        private lateinit var controller: TrackController
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            controller = TrackController(this)
+            initViews()
+        }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        fun initViews() {
+            txtSearch = findViewById(R.id.txtSearch)
+            btnSearch = findViewById(R.id.btnSearch)
+            listViewItems = findViewById(R.id.listViewItems)
+            btnSearch.setOnClickListener {
+                val query = txtSearch.text.toString()
+                controller.searchTracks(query)
+            }
+        }
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+        fun setTrackAdapter(adapter: TrackAdapter) {
+            listViewItems?.adapter = adapter
         }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
-}
--- app/src/main/java/cr/ac/una/spoty/controllers/TrackController.kt
+++ app/src/main/java/cr/ac/una/spoty/controllers/TrackController.kt
@@ -1,2 +1,167 @@
 package cr.ac.una.spoty.controllers
 
+
+import android.util.Base64
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import androidx.room.Room
+import cr.ac.una.spoty.MainActivity
+import cr.ac.una.spoty.R
+import cr.ac.una.spoty.dao.SearchQueryDao
+import cr.ac.una.spoty.db.AppDatabase
+import cr.ac.una.spoty.entity.*
+import cr.ac.una.spoty.service.SpotifyService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class TrackController(private val activity: MainActivity) {
+
+        private val trackList: MutableList<Track> = mutableListOf()
+        private lateinit var trackAdapter: TrackAdapter
+
+        // Crear la instancia de la base de datos Room
+        private lateinit var searchQueryDao: SearchQueryDao
+        private lateinit var db: AppDatabase
+        private lateinit var btnSearch: Button
+        private lateinit var txtSearch: EditText
+        private var listViewItems: ListView? = null
+
+
+    private val spotifyServiceToken: SpotifyService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://accounts.spotify.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(SpotifyService::class.java)
+    }
+    private val spotifyService: SpotifyService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.spotify.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(SpotifyService::class.java)
+    }
+
+    private fun initViews() {
+        txtSearch = activity.findViewById(R.id.txtSearch)
+        btnSearch = activity.findViewById(R.id.btnSearch)
+        listViewItems = activity.findViewById(R.id.listViewItems)
+        btnSearch.setOnClickListener {
+            val query = txtSearch.text.toString()
+            searchTracks(query)
+        }
+    }
+
+    init {
+            initViews()
+            trackAdapter = TrackAdapter(activity.applicationContext, trackList)
+            listViewItems?.adapter = trackAdapter
+
+            // Crear la instancia de la base de datos Room
+            db = Room.databaseBuilder(
+                activity.applicationContext,
+                AppDatabase::class.java, "spoty-db"
+            ).build()
+            searchQueryDao = db.searchQueryDao()
+        }
+
+        fun searchTracks(query: String) {
+            val clientId = "f710fecf58e242bd8054929a50a08728" /* f13969da015a4f49bb1f1edef2185d4e*/
+            val clientSecret = "bb3621a5cb6f402bbb543e2d97c7cd29" /*e3077426f4714315937111d5e82cd918*/
+            val base64Auth = Base64.encodeToString("$clientId:$clientSecret".toByteArray(), Base64.NO_WRAP)
+
+            val tokenRequest = spotifyServiceToken.getAccessToken(
+                "Basic $base64Auth",
+                "client_credentials"
+            )
+
+
+            tokenRequest.enqueue(object : Callback<AccessTokenResponse> {
+                override fun onResponse(call: Call<AccessTokenResponse>, response: Response<AccessTokenResponse>) {
+                    if (response.isSuccessful) {
+                        val accessTokenResponse = response.body()
+                        val accessToken = accessTokenResponse?.accessToken
+
+                        if (accessToken != null) {
+                            val searchRequest = spotifyService.searchTrack("Bearer $accessToken", query)
+                            searchRequest.enqueue(object : Callback<TrackResponse> {
+                                override fun onResponse(call: Call<TrackResponse>, response: Response<TrackResponse>) {
+                                    if (response.isSuccessful) {
+                                        val trackResponse = response.body()
+
+                                        if (trackResponse != null && trackResponse.tracks.items.isNotEmpty()) {
+
+                                            trackList.clear()
+                                            var count = 0 // Variable de conteo para limitar la cantidad de canciones a mostrar
+
+                                            for (track in trackResponse.tracks.items) {
+                                                val trackName = track.name
+                                                val artistName = track.album.name
+                                                val trackUri = track.uri
+                                                val imageUrl = track.album.images.firstOrNull()?.url ?: ""
+
+                                                //displayTrackInfo(trackName, artistName, track.album.images[0].url)
+                                                displayTrackInfo(trackName, artistName, imageUrl)
+                                                count++ // Incrementar el conteo
+
+                                                if (count >= 50) {
+                                                    break // Detener el bucle después de 50 canciones
+                                                }
+                                            }
+                                            // Guardar la búsqueda en la base de datos local
+                                            val searchQuery = SearchQuery(query = query)
+                                            saveSearchQuery(searchQuery)
+                                        } else {
+                                            displayErrorMessage("No se encontraron canciones.")
+                                        }
+                                    } else {
+                                        displayErrorMessage("Error en la respuesta del servidor.")
+                                    }
+                                }
+
+                                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                                    displayErrorMessage("Error en la solicitud de búsqueda.")
+                                }
+                            })
+                        } else {
+                            displayErrorMessage("Error al obtener el accessToken.")
+                        }
+                    } else {
+                        displayErrorMessage("Error en la respuesta del servidor.")
+                    }
+                }
+
+                override fun onFailure(call: Call<AccessTokenResponse>, t: Throwable) {
+                    displayErrorMessage("Error en la solicitud de accessToken.")
+                }
+            })
+        }
+
+    private fun displayTrackInfo(trackName: String, artistName: String, imageUrl: String) {
+        val album = Album(artistName, listOf(Image(imageUrl, 0, 0)))
+        val track = Track(trackName, album, "")
+        trackList.add(track)
+        trackAdapter.notifyDataSetChanged()
+    }
+
+    private fun saveSearchQuery(searchQuery: SearchQuery) {
+        // Utilizar una corutina para realizar la operación de inserción en segundo plano
+        CoroutineScope(Dispatchers.IO).launch {
+            searchQueryDao.insertSearchQuery(searchQuery)
+        }
+    }
+    private fun displayErrorMessage(errorMessage: String) {
+        Toast.makeText(activity, errorMessage, Toast.LENGTH_SHORT).show()
+    }
+
+}
