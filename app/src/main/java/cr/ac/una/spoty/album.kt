ccc9c0d8fb10925f85a34478339dca1cfa929564
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-06-08 15:48:12 -0600

    agrego navegacion del menu

---
+++ app/src/main/java/cr/ac/una/spoty/album.kt
@@ -0,0 +1,11 @@
+package cr.ac.una.spoty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class album : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album)
+    }
+}
--- app/src/main/java/cr/ac/una/spoty/entity/trackAdaprter.kt
+++ app/src/main/java/cr/ac/una/spoty/entity/trackAdaprter.kt
@@ -1,6 +1,7 @@
 package cr.ac.una.spoty.entity
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,8 @@ import android.widget.PopupMenu
 import android.widget.TextView
 import com.squareup.picasso.Picasso
 import cr.ac.una.spoty.R
+import cr.ac.una.spoty.album
+import cr.ac.una.spoty.artista
 
 
 class TrackAdapter(context: Context, tracks: List<Track>) : ArrayAdapter<Track>(context, 0, tracks) {
@@ -28,22 +31,36 @@ class TrackAdapter(context: Context, tracks: List<Track>) : ArrayAdapter<Track>(
 
         txtTrackName?.text = track?.name
         txtArtistName?.text = track?.album?.name
-
         Picasso.get().load(track?.album?.images?.get(0)?.url).into(imgPhoto)
+
+
+        //OPCIONES DEL MENU
         val imgMenu = convertView?.findViewById<ImageView>(R.id.imgAction)
         imgMenu?.setOnClickListener {
             val popupMenu = PopupMenu(context, imgMenu)
             popupMenu.inflate(R.menu.main_menu)
-
             // Agrega clics de escucha para las opciones del menú
             popupMenu.setOnMenuItemClickListener { menuItem ->
                 when (menuItem.itemId) {
                     R.id.menu_view_album -> {
                         // Acción para Ver álbum
+                        val intent = Intent(context, album::class.java)
+
+                        val appContext = context.applicationContext
+                        //e asegura que la actividad se inicie en una nueva tarea independiente en lugar de agregarse a la tarea existente.
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        appContext.startActivity(intent)
                         true
                     }
                     R.id.menu_view_artis -> {
                         // Acción para Ver artista
+                        val intent = Intent(context, artista::class.java)
+
+                        val appContext = context.applicationContext
+                        //e asegura que la actividad se inicie en una nueva tarea independiente en lugar de agregarse a la tarea existente.
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        appContext.startActivity(intent)
+
                         true
                     }
                     else -> false
